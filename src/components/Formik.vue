19ce0c3f009e4aabc5e245ccd44527fbb435669e
Samy Hamed-e-saberi <samy.saberi@gmail.com>
2022-12-14 15:25:25 +0100

    formik

--- src/components/Formik.vue
+++ src/components/Formik.vue
@@ -3,13 +3,12 @@
 
   const values = reactive({})
   const errors = reactive({})
-  const handleSubmit = reactive({})
   const isSubmitting = reactive({})
 
   const $emit = defineEmits({
     submit: (data) => {
       return(
-          data
+          data !== null
       )
     }
   })
@@ -18,15 +17,25 @@
     $emit('submit', values)
   }
 
-  function validate(values) {
+//validate function
+  const validate = (values) => {
+    const errors = {}
     if (!values.email) {
-      this.errors.email = 'Required'
+      errors.email = 'Required'
     } else if (
       !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
     ) {
       errors.email = 'Invalid email address'
     }
     return errors
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    errors = validate(values)
+    if (Object.keys(errors).length === 0) {
+      onSubmit(values)
+    }
   }
 </script>
 
