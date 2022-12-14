81275f57e5f2c903bcbdc399f0b30afcffc52018
Théo Sigaud <sigaudtheo@gmail.com>
2022-12-14 15:46:57 +0100

    Add return

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <script setup>
 import Formik from '@/components/Formik.vue'
+
+const values = reactive({})
+const errors = reactive({})
+const handleSubmit = reactive({})
+const isSubmitting = reactive({})
+
+
 </script>
 
 <template>
-  <Formik :initialValues="['email', 'password']" :validate="'zdzd'"/>
+  <Formik :initialValues="['email', 'password']" :validate="'zdzd'" :onSubmit="'r" @values="values" @errors="errors"/>
 </template>
